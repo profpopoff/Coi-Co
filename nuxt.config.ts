a8b9d8d0a433175dbc2ec5e9c107510952e18fcf
Aleksei Ivanov <profpopoff@gmail.com>
2024-08-08 18:30:24 +0300

    CNC-5: Change browser icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,39 @@ export default defineNuxtConfig({
           name: "description",
           content: "Coi&Co is a multi-page entertainment web app.",
         },
+        {
+          name: "msapplication-TileColor",
+          content: "#f74943",
+        },
+        {
+          name: "theme-color",
+          content: "#ffffff",
+        },
+      ],
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/favicon/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon/favicon-16x16.png",
+        },
+        { rel: "manifest", href: "/favicon/site.webmanifest" },
+        {
+          rel: "mask-icon",
+          href: "/favicon/safari-pinned-tab.svg",
+          color: "#5bbad5",
+        },
       ],
     },
   },
